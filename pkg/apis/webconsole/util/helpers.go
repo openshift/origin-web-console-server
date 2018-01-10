5ec159bda3aa8eee4a393a5bdbf17e54b73fafc7
Samuel Padgett <spadgett@redhat.com>
2018-01-10 17:08:37 -0500

    Validate extension scripts and stylesheets as URLs
    Also remove obsolete extensions directories array, which is no longer used.

--- pkg/apis/webconsole/util/helpers.go
+++ pkg/apis/webconsole/util/helpers.go
@@ -17,15 +17,5 @@ func GetWebConsoleFileReferences(config *v1.WebConsoleConfiguration) []*string {
 		refs = append(refs, &config.ServingInfo.NamedCertificates[i].KeyFile)
 	}
 
-	for i := range config.ExtensionScripts {
-		refs = append(refs, &config.ExtensionScripts[i])
-	}
-	for i := range config.ExtensionStylesheets {
-		refs = append(refs, &config.ExtensionStylesheets[i])
-	}
-	for i := range config.Extensions {
-		refs = append(refs, &config.Extensions[i].SourceDirectory)
-	}
-
 	return refs
 }
